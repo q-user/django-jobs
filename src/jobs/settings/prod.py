250e32493a062fa06c58f32d520fde549238dbe2
Mikhail Pavlov <pavlov.mikhail@protonmail.com>
2020-05-11 11:26:35 +0300

    Adds production cache settings

--- src/jobs/settings/prod.py
+++ src/jobs/settings/prod.py
@@ -21,3 +21,13 @@ CELERY_BROKER_URL = os.environ.get('REDIS_URL_0', 'redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': os.environ.get('REDIS_URL_1', 'redis://localhost:6379/1'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
