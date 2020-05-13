17cf4cd5c341ea42905626c48496b95509a97935
Mikhail Pavlov <pavlov.mikhail@protonmail.com>
2020-05-13 12:30:06 +0300

    Sort sources by latest article saved

--- src/aggregator/views.py
+++ src/aggregator/views.py
@@ -1,12 +1,14 @@
 from datetime import date, timedelta
 
 from django.core.exceptions import PermissionDenied
-from django.db.models import Count, Q
+from django.db.models import Count, Q, OuterRef, Subquery
+from django.db.models.functions import Coalesce
 from django.http import JsonResponse
 from django.utils.module_loading import import_string
 from django.views.generic import TemplateView
 
 from aggregator.models import DataSource
+from articles.models import Article
 
 
 def ajax_plugin_configuration(request):
@@ -25,10 +27,13 @@ class StatsView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         week_delta = date.today() - timedelta(days=7)
+        newest = Article.objects.filter(source=OuterRef('pk')).order_by('-timestamp')
+        sources = DataSource.objects.annotate(
+            week_count=Count('articles', filter=Q(articles__timestamp__gte=week_delta)),
+            total_count=Count('articles'),
+            newest_article_time=Coalesce(Subquery(newest.values('timestamp')[:1]), date(1, 1, 1))
+        ).order_by('-newest_article_time')
         context.update({
-            "data_sources": DataSource.objects.annotate(
-                week_count=Count('articles', filter=Q(articles__timestamp__gte=week_delta)),
-                total_count=Count('articles'),
-            ).order_by('-week_count')
+            "data_sources": sources
         })
         return context
