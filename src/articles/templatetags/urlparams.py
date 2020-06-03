7422294a7f1fe9d8e42207bf90a0c3378d0e2d32
Mikhail Pavlov <pavlov.mikhail@protonmail.com>
2020-06-03 18:30:19 +0300

    Adds remove_query_params simple_tag

--- src/articles/templatetags/urlparams.py
+++ src/articles/templatetags/urlparams.py
@@ -9,3 +9,11 @@ def add_query_params(request, **kwargs):
     for k, v in kwargs.items():
         updated[k] = v
     return request.build_absolute_uri('?' + updated.urlencode())
+
+
+@register.simple_tag
+def remove_query_params(request, *args):
+    updated = request.GET.copy()
+    for arg in args:
+        updated.pop(arg, None)
+    return request.build_absolute_uri('?' + updated.urlencode())
